4dbc8cc312b8d75741b9edee4da23984100e75ae
Pettergon <henrik@hpetter.de>
2020-11-20 20:50:54 +0100

    init

---
+++ Präsenz/Search.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Search {
+
+    public static void main(String[] args){
+        int[] a = { 931046, 931051, 931193, 931431, 931434, 931560, 931579, 931602, 931717, 931984, 932056, 932089, 932127, 932219, 932303, 932444, 932559, 932714, 932747, 932788};
+        int searchedNumber = 931579;
+
+        System.out.println( "(Linear) Number was found at index: "+ linearSearch(a, searchedNumber));
+        System.out.println( "(Binary) Number was found at index: "+ binarySearch(a, 0, a.length, searchedNumber));
+
+    }
+
+    public static int linearSearch(int[] a, int nr){
+        for (int i = 0; i < a.length; i++){
+            if(a[i] == nr){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearch(int[] a,int left, int right, int nr){
+        int mid = (left+right)/2;
+        if(a[mid] == nr){
+            return mid;
+        }else if(a[mid] < nr){
+            return binarySearch(a, mid+1, right, nr);
+        }else{
+            return binarySearch(a, left, mid, nr);
+        }
+    }
+}
