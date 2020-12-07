3994d04fa790c32df1b264ec52bace5735cb5829
Pettergon <henrik@hpetter.de>
2020-12-07 19:56:27 +0100

    Added iterative version of binarysearch

--- Präsenz/Search.java
+++ Präsenz/Search.java
@@ -2,10 +2,11 @@ public class Search {
 
     public static void main(String[] args){
         int[] a = { 931046, 931051, 931193, 931431, 931434, 931560, 931579, 931602, 931717, 931984, 932056, 932089, 932127, 932219, 932303, 932444, 932559, 932714, 932747, 932788};
-        int searchedNumber = 93159;
+        int searchedNumber = 9317717;
 
         System.out.println( "(Linear) Number was found at index: "+ linearSearch(a, searchedNumber));
-        System.out.println( "(Binary) Number was found at index: "+ binarySearch(a, 0, a.length - 1, searchedNumber));
+        System.out.println( "(Binary recursive) Number was found at index: "+ binarySearch(a, 0, a.length-1, searchedNumber));
+        System.out.println( "(Binary iteratively) Number was found at index: "+ binarySearchIterative(a, searchedNumber));
 
     }
 
@@ -27,9 +28,25 @@ public class Search {
             return -1;
         }
         if(a[mid] < nr){
-            return binarySearch(a, mid+1, right, nr);
+            return binarySearch(a, mid + 1, right, nr);
         }else{
-            return binarySearch(a, left, mid, nr);
+            return binarySearch(a, left, mid -1 , nr);
         }
+    }
+
+    public static int binarySearchIterative(int[] a, int nr){
+        int left = 0;
+        int right = a.length -1;
+        while (left <= right){
+            int mid = (left + right) / 2;
+            if(a[mid] == nr){
+                return mid;
+            }else if(a[mid] < nr){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return -1;
     }
 }
